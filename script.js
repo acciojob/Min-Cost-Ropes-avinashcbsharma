8fcdb25f270771a9bfcf68692ebc9c338d96d1c5
avinashcbsharma <156292691+avinashcbsharma@users.noreply.github.com>
2024-02-14 16:08:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,53 @@
 function mincost(arr)
 { 
-//write your code here
-// return the min cost
+function mincost(arr) {
+    // Convert the input array to a min-heap
+    const heapify = (array) => {
+      for (let i = Math.floor(array.length / 2); i >= 0; i--) {
+        heapifyDown(array, i);
+      }
+    };
+  
+    const heapifyDown = (array, i) => {
+      const left = 2 * i + 1;
+      const right = 2 * i + 2;
+      let smallest = i;
+  
+      if (left < array.length && array[left] < array[smallest]) {
+        smallest = left;
+      }
+  
+      if (right < array.length && array[right] < array[smallest]) {
+        smallest = right;
+      }
+  
+      if (smallest !== i) {
+        [array[i], array[smallest]] = [array[smallest], array[i]];
+        heapifyDown(array, smallest);
+      }
+    };
+  
+    heapify(arr);
+  
+   
+    let totalCost = 0;  
+   
+    while (arr.length > 1) {
+     
+      const rope1 = arr.shift();
+      const rope2 = arr.shift();
+       
+      const cost = rope1 + rope2;
+       
+      totalCost += cost;
+       
+      arr.push(cost);
+      heapifyDown(arr, 0);
+    }
+  
+    return totalCost;
+  }
+  
   
 }
 
